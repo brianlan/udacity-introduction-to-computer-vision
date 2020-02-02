ab5419bf54b183f60ed35aa783d495876ec87b91
brianlan <brianlanbo@gmail.com>
2020-02-02 11:54:12 +0800

    created a function in helpers.py to read image, and refactored all ipynb

--- helpers.py
+++ helpers.py
@@ -1,3 +1,16 @@
+from PIL import Image
+import numpy as np
+
+
+def read_image(path, as_ndarray=True, convert_grayscale=False):
+    im = Image.open(path)
+    if convert_grayscale:
+        im = im.convert("L")
+    if as_ndarray:
+        im = np.array(im)
+    return im
+
+
 def plot_image(im, ax, title=None, **kwargs):
     ax.set_axis_off()
     ax.imshow(im, **kwargs)
